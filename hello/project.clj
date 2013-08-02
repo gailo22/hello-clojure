99d709c259c2a969fc8bc33a364634e6eafd681f
gailo22 <gailo22@gmail.com>
2013-08-02 10:59:15 +0700

    update

--- hello/project.clj
+++ hello/project.clj
@@ -1,5 +1,4 @@
 (defproject hello "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]]
+  :dependencies [[org.clojure/clojure "1.5.0"]]
   :main hello.core)
-  
